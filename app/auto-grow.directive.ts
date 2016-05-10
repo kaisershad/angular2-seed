5de5bd6e1a6deac939c968c2568085a94d7303a0
kaisershad <kaisershad@gmail.com>
2016-05-10 17:07:16 +0200

    Add files via upload

---
+++ app/auto-grow.directive.ts
@@ -0,0 +1,26 @@
+import {Directive} from 'angular2/core'
+import {ElementRef} from "angular2/core"
+import {Renderer} from "angular2/core"
+
+@Directive({
+    selector: '[autoGrow]',
+    host: {
+        '(focus)': 'onFocus()',
+        '(blur)': 'onBlur()'
+    }
+})
+
+export class AutoGrowDirective {
+
+    constructor(private el: ElementRef, private renderer: Renderer){
+
+    }
+
+    onFocus(){
+        this.renderer.setElementStyle(this.el.nativeElement, 'width', '200')
+    }
+
+    onBlur(){
+        this.renderer.setElementStyle(this.el.nativeElement, 'width', '120')
+    }
+}
